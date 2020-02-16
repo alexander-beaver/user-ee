07b2dacd262339afcf05bdec38335cbf3b88673d
Alexander Beaver <alex@beaver.dev>
2020-02-16 14:06:04 -0800

    Modify db.go

--- core/db/db.go
+++ core/db/db.go
@@ -20,19 +20,16 @@ func SetupDatabase() *gorm.DB {
 }
 
 
-func writeErrorToDB(reported Error){
-
+func WriteErrorToDB(db *gorm.DB, reported _struct.Error){
+	db.Create(reported)
 
 
 }
 
-func getEntryFromDB(id uint16) Error {
-	var entry Error
-	db, err := gorm.Open("sqlite3", dbName)
-	if(err != nil){
-		panic("failed to connect database")
+func GetEntryFromDBGivenID(db *gorm.DB, id uint16) _struct.Error {
+	var entry _struct.Error
 
-	}
+
 
 	db.Where("name = ?", id).Find(&entry)
 	return entry
