2fe2a997f188e1f281659f5a71a2b115562a1e9e
Alexander Beaver <alex@beaver.dev>
2020-02-16 15:14:25 -0800

    Add RespondOK

--- app.go
+++ app.go
@@ -1,27 +1,29 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/alexander-beaver/user-ee/api"
 	"github.com/alexander-beaver/user-ee/core/db"
-	_struct "github.com/alexander-beaver/user-ee/core/struct"
 	"log"
 	"net/http"
-	"time"
 )
 
 
 func main() {
 	sqlite := db.SetupDatabase()
+	/*
 	db.WriteErrorToDB(sqlite, _struct.Error{
 		EndpointID:   "123",
 		ErrorID:      0,
 		ErrorMessage: "123",
 		Comments:     "123",
 		Time:         time.Now(),
-	})
+	})*/
 
 	//fmt.Println(json.Marshal(db.GetEntryFromDBGivenID(sqlite, 0)))
-
+	res, _ := json.Marshal(db.GetAllEntriesFromDB(sqlite))
+	fmt.Println(string(res))
 	http.HandleFunc("/",api.PutErrorAPIHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
--- core/webserver.go
+++ core/webserver.go
@@ -1,7 +1,29 @@
 package core
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 type Response struct{
 	Status uint16
-	Body struct{}
+	Body interface{}
 }
 
+func RespondOK(w http.ResponseWriter, r *http.Request, content interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	res := Response{
+		Status: http.StatusOK,
+		Body:   content,
+	}
+
+	json, err := json.Marshal(res)
+	if(err != nil){
+		panic(err)
+	}
+	fmt.Fprintf(w, string(json))
+
+}
